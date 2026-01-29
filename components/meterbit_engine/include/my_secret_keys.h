3ce51f60114075689d9408c8340e3798e0dbb77b
Meterbit <meterbitcyb@gmail.com>
2026-01-29 23:02:40 +0100

    Update my secret keys file

--- components/meterbit_engine/include/my_secret_keys.h
+++ components/meterbit_engine/include/my_secret_keys.h
@@ -5,14 +5,14 @@
 
 // Uncomment all and fill in your keys below (depending on which services you use).
 
-static const char github_Token[] = "place your github PAT token here";  //get an api key from your github account settings
-static const char openai_key[] = "place your openAI API key here";  //get free key @ https://platform.openai.com/account/api-keys
-static const char googleClient_ID[] = "place your Google Cloud App CLIENT ID here";  //get free key @ https://console.cloud.google.com/apis/credentials
-static const char googleClient_SECRET[] = "place your Google Cloud App CLIENT SECRET here";   //get free key @ https://console.cloud.google.com/apis/credentials
-static const char outlookClient_ID[] = "insert your outlook cliend ID here";    //get free key @ https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
-static const char outlookClient_SECRET[] = "insert your outlook client secret here";    //get free key @ https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
-static const char *api_fooball_token_key = "place your api-football api key here";    //get free key @ https://www.api-football.com/pricing
-static const char spotifyClient_ID[] = "place your Spotify Api key here";     // Your client ID of your spotify APP
-static const char finnhubApiKey[] = "place your finnhub api key here"; //get free key @ finnhub.io/register
+static const char github_Token[] = "place your github PAT token here ";  //get an api key from your github account settings
+static const char openai_key[] = "place your openAI API key here ";  //get free key @ https://platform.openai.com/account/api-keys
+static const char googleClient_ID[] = "place your Google Cloud App CLIENT ID here ";  //get free key @ https://console.cloud.google.com/apis/credentials
+static const char googleClient_SECRET[] = "place your Google Cloud App CLIENT SECRET here ";   //get free key @ https://console.cloud.google.com/apis/credentials
+static const char outlookClient_ID[] = "insert your outlook cliend ID here ";    //get free key @ https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
+static const char outlookClient_SECRET[] = "insert your outlook client secret here ";    //get free key @ https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
+static const char *api_fooball_token_key = "place your api-football api key here ";    //get free key @ https://www.api-football.com/pricing
+static const char spotifyClient_ID[] = "place your Spotify Api key here ";     // Your client ID of your spotify APP
+static const char finnhubApiKey[] = "place your finn hub api key here "; //get free key @ finnhub.io/register
 
 #endif
